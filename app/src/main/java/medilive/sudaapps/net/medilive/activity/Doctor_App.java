ccde8ca1a40a36e4e4216b0ed055a162fe295646
Muawia Ibrahim Bashir <m.i.bashir1982@gmail.com>
2015-11-24 14:04:36 +0300

    update Doctors reservation activity

--- app/src/main/java/medilive/sudaapps/net/medilive/activity/Doctor_App.java
+++ app/src/main/java/medilive/sudaapps/net/medilive/activity/Doctor_App.java
@@ -1,14 +1,13 @@
 package medilive.sudaapps.net.medilive.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,79 +21,85 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import medilive.sudaapps.net.medilive.R;
+import medilive.sudaapps.net.medilive.app.AppConfig;
 
 /**
  * Created by muawia.ibrahim on 10/12/2015.
  */
-public class Doctor_App extends FragmentActivity implements Spinner.OnItemSelectedListener {
-    public static final String DATA_URL = "http://sudaapps.net/android/medilife/food_api/spinnerfill.php";
-
-    //Tags used in the JSON String
-    public static final String TAG_USERNAME = "doc_sp";
-    public static final String TAG_NAME = "doc_name";
-    public static final String TAG_COURSE = "time_av";
-    public static final String TAG_SESSION = "address";
-    //JSON array name
-    public static final String JSON_ARRAY = "result";
+public class Doctor_App extends AppCompatActivity implements Spinner.OnItemSelectedListener {
     //Declaring an Spinner
-    //JSON Array
-    private JSONArray result;
-    private Spinner spinner,doc_name;
-    private TextView tim_av;
+    private Spinner spinner;
 
     //An ArrayList for Spinner Items
     private ArrayList<String> students;
+
+    //JSON Array
+    private JSONArray result;
+
+    //TextViews to display details
+    private TextView textViewName;
+    private TextView textViewCourse;
+    private TextView textViewSession;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctor_app);
+        //Initializing the ArrayList
+        //Initializing the ArrayList
         students = new ArrayList<String>();
 
         //Initializing Spinner
-        spinner = (Spinner) findViewById(R.id.spec);
-        doc_name=(Spinner)findViewById(R.id.doc_name);
-        tim_av=(TextView)findViewById(R.id.tim_av);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
         //Adding an Item Selected Listener to our Spinner
         //As we have implemented the class Spinner.OnItemSelectedListener to this class iteself we are passing this to setOnItemSelectedListener
         spinner.setOnItemSelectedListener(this);
+spinner.setPrompt("اختار اخصائي");
+        //Initializing TextViews
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewCourse = (TextView) findViewById(R.id.textViewCourse);
+        textViewSession = (TextView) findViewById(R.id.textViewSession);
 
-    
+        //This method will fetch the data from the URL
         getData();
-
     }
 
-    private void getData() {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, DATA_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JSONObject j = null;
-                try {
-                    //Parsing the fetched Json String to JSON Object
-                    j = new JSONObject(response);
+    private void getData(){
+        //Creating a string request
+        StringRequest stringRequest = new StringRequest(AppConfig.DATA_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject j = null;
+                        try {
+                            //Parsing the fetched Json String to JSON Object
+                            j = new JSONObject(response);
 
-                    //Storing the Array of JSON String to our JSON Array
-                    result = j.getJSONArray(JSON_ARRAY);
+                            //Storing the Array of JSON String to our JSON Array
+                            result = j.getJSONArray(AppConfig.JSON_ARRAY);
 
-                    //Calling method getStudents to get the students from the JSON Array
-                    getStudents(result);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                            //Calling method getStudents to get the students from the JSON Array
+                            getSpeci(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+                    }
+                });
 
-            }
-        });
         //Creating a request queue
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         //Adding request to the queue
         requestQueue.add(stringRequest);
     }
-    private void getStudents(JSONArray j){
+
+    private void getSpeci(JSONArray j){
         //Traversing through all the items in the json array
         for(int i=0;i<j.length();i++){
             try {
@@ -102,7 +107,7 @@ public class Doctor_App extends FragmentActivity implements Spinner.OnItemSelect
                 JSONObject json = j.getJSONObject(i);
 
                 //Adding the name of the student to array list
-                students.add(json.getString(TAG_USERNAME));
+                students.add(json.getString(AppConfig.TAG_DOC_SP));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -113,55 +118,61 @@ public class Doctor_App extends FragmentActivity implements Spinner.OnItemSelect
     }
 
     //Method to get student name of a particular position
-  //private String getName(int position){
-   //    String doc_name="";
-   //    try {
+   private String getName(int position){
+        String name="";
+      try {
             //Getting object of given index
-   //      JSONObject json = result.getJSONObject(position);
+          JSONObject json = result.getJSONObject(position);
 
             //Fetching name from that object
-   //        doc_name = json.getString(TAG_NAME);
-   //    } catch (JSONException e) {
-    //       e.printStackTrace();
-   //    }
+          name = json.getString(AppConfig.TAG_ADDRESS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+       }
         //Returning the name
-   //    return doc_name;
-// }
-    //Doing the same with this method as we did with getName()
- ///  private String getTime_av(int position){
-   // String Time_av="";
-
-   //    try {
-    //       JSONObject json = result.getJSONObject(position);
-       //    Time_av = json.getString(TAG_COURSE);
-     //  } catch (JSONException e) {
-     //      e.printStackTrace();
-    //    }
-     //   return Time_av;
-    //}
+       return name;
+  }
 
     //Doing the same with this method as we did with getName()
-  //  private String getSession(int position){
-  //      String session="";
-  //      try {
-  //          JSONObject json = result.getJSONObject(position);
-  //          session = json.getString(TAG_SESSION);
-  //      } catch (JSONException e) {
-  //          e.printStackTrace();
- //       }
- //       return session;
- //   }
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-    // tim_av.setText(getTime_av(position));
-      // doc_name.setPrompt(getName(position));
-
+    private String getCourse(int position){
+        String course="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            course = json.getString(AppConfig.TAG_DOC_NAME);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return course;
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    //Doing the same with this method as we did with getName()
+    private String getSession(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(AppConfig.TAG_DOC_TIME);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
 
+
+    //this method will execute when we pic an item from the spinner
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Setting the values to textviews for a selected item
+//        textViewName.setText(getName(position));
+        textViewName.setText(getCourse(position));
+        textViewCourse.setText(getName(position));
+        textViewSession.setText(getSession(position));
+    }
+
+    //When no item is selected this method would execute
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        textViewName.setText("");
+        textViewCourse.setText("");
+        textViewSession.setText("");
     }
 }
